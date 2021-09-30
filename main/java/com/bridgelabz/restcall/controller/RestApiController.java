8476273ead886fa3030da0ecf15a13f1173aa67f
Vaibhav Kokate <vaibhav.kokate@morningstar.com>
2021-10-01 03:56:08 +0530

    [Vaibhav] added ability to make rest call and pass name as path variable

--- main/java/com/bridgelabz/restcall/controller/RestApiController.java
+++ main/java/com/bridgelabz/restcall/controller/RestApiController.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.restcall.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +17,11 @@ public class RestApiController {
 	
 	@RequestMapping(value = {"/query"}, method = RequestMethod.GET)
 	public String hello(@RequestParam(value = "name") String name) {
+		return "Hello " + name + "!";
+	}
+	
+	@GetMapping("/param/{name}")
+	public String helloParam(@PathVariable String name) {
 		return "Hello " + name + "!";
 	}
 }
