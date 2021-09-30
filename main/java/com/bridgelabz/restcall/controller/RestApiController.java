391779b70df31d9df0b736e615086823d07125b6
Vaibhav Kokate <vaibhav.kokate@morningstar.com>
2021-10-01 03:59:40 +0530

    [Vaibhav] added ability to make rest call and pass firstname and lastname

--- main/java/com/bridgelabz/restcall/controller/RestApiController.java
+++ main/java/com/bridgelabz/restcall/controller/RestApiController.java
@@ -2,10 +2,14 @@ package com.bridgelabz.restcall.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bridgelabz.restcall.model.User;
 
 @RestController
 @RequestMapping("/hello")
@@ -23,5 +27,10 @@ public class RestApiController {
 	@GetMapping("/param/{name}")
 	public String helloParam(@PathVariable String name) {
 		return "Hello " + name + "!";
+	}
+	
+	@PostMapping("/post")
+	public String hello(@RequestBody User user) {
+		return "Hello " + user.getFirstName() + " " + user.getLastName()+"!";
 	}
 }
---
+++ main/java/com/bridgelabz/restcall/model/User.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.restcall.model;
+
+public class User {
+	private String firstName;
+	private String lastName;
+
+	public String getFirstName() {
+		
+		return firstName;
+	}
+
+	public String getLastName() {
+		
+		return lastName;
+	}
+}
