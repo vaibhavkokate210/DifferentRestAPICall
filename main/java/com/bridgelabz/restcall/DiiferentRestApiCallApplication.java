63d252806bda7c2cfae49b64fdc41e909a572e16
Vaibhav Kokate <vaibhav.kokate@morningstar.com>
2021-10-01 03:45:15 +0530

    [Vaibhav] added initial setup commit

---
+++ main/java/com/bridgelabz/restcall/DiiferentRestApiCallApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.restcall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiiferentRestApiCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiiferentRestApiCallApplication.class, args);
+	}
+
+}
---
+++ test/java/com/bridgelabz/restcall/DiiferentRestApiCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.restcall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiiferentRestApiCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
